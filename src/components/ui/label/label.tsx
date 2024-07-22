23bb8586bc1db5644891f26214169f60a27388b1
harshmangalam <harshdev8218@gmail.com>
2024-07-22 14:28:39 +0530

    added Label componnet

---
+++ src/components/ui/label/label.tsx
@@ -0,0 +1,19 @@
+import { component$, Slot, PropsOf } from "@builder.io/qwik";
+import { Label as HeadlessLabel } from "@qwik-ui/headless";
+import { cn } from "@qwik-ui/utils";
+
+type LabelProps = PropsOf<"label">;
+
+export const Label = component$<LabelProps>((props) => {
+  return (
+    <HeadlessLabel
+      {...props}
+      class={cn(
+        "font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        props.class,
+      )}
+    >
+      <Slot />
+    </HeadlessLabel>
+  );
+});
