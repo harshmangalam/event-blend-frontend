ec47bb796681765b1ea210b0bc9c1cec959e9656
harshmangalam <harshdev8218@gmail.com>
2024-07-22 14:29:22 +0530

    added Label componnet

---
+++ src/components/ui/separator/separator.tsx
@@ -0,0 +1,22 @@
+import { PropsOf, component$ } from "@builder.io/qwik";
+import { Separator as HeadlessSeparator } from "@qwik-ui/headless";
+import { cn } from "@qwik-ui/utils";
+
+export const Separator = component$<PropsOf<typeof HeadlessSeparator>>(
+  ({ orientation = "horizontal", decorative = true, ...props }) => {
+    return (
+      <>
+        <HeadlessSeparator
+          {...props}
+          decorative={decorative}
+          orientation={orientation}
+          class={cn(
+            "shrink-0 bg-border",
+            orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+            props.class,
+          )}
+        />
+      </>
+    );
+  },
+);
