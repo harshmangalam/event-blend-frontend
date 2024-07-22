04e688781786e107c38694e795274286bee86b83
harshmangalam <harshdev8218@gmail.com>
2024-07-22 14:27:29 +0530

    added Input componnet

---
+++ src/components/ui/input/input.tsx
@@ -0,0 +1,45 @@
+import { $, component$, type PropsOf } from "@builder.io/qwik";
+import { cn } from "@qwik-ui/utils";
+
+type InputProps = PropsOf<"input"> & {
+  error?: string;
+};
+
+export const Input = component$<InputProps>(
+  ({
+    name,
+    error,
+    id,
+    ["bind:value"]: valueSig,
+    value,
+    onInput$,
+    ...props
+  }) => {
+    const inputId = id || name;
+
+    return (
+      <>
+        <input
+          {...props}
+          aria-errormessage={`${inputId}-error`}
+          aria-invaid={!!error}
+          // workaround to support two way data-binding on the Input component (https://github.com/QwikDev/qwik/issues/3926)
+          value={valueSig ? valueSig.value : value}
+          onInput$={
+            valueSig ? $((__, el) => (valueSig.value = el.value)) : onInput$
+          }
+          class={cn(
+            "flex h-12 w-full rounded-base border border-input bg-background px-3 py-1 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            props.class,
+          )}
+          id={inputId}
+        />
+        {error && (
+          <div id={`${inputId}-error`} class="text-destructive mt-1 text-sm">
+            {error}
+          </div>
+        )}
+      </>
+    );
+  },
+);
