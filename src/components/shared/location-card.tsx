76d949e4341a48a8acd54b4c68efcec6ed7374d2
harshmangalam <harshdev8218@gmail.com>
2024-07-25 23:45:20 +0530

    added shared location card

---
+++ src/components/shared/location-card.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+import { LuBuilding2 } from "@qwikest/icons/lucide";
+import { Card } from "~/components/ui/card/card";
+import { Badge } from "~/components/ui/badge/badge";
+import { Link } from "@builder.io/qwik-city";
+import { type Location } from "~/lib/types";
+export const LocationCard = component$(
+  ({ location }: { location: Pick<Location, "id" | "city" | "_count"> }) => {
+    const { id, city, _count } = location;
+    return (
+      <Link key={id} href={`/cities/${id}`}>
+        <Card.Root class="max-w-md">
+          <Card.Content class="m-0 flex flex-col items-center gap-3 p-4">
+            <div class="grid h-36 w-36 place-items-center rounded-full bg-muted">
+              <LuBuilding2 class="h-16 w-16 text-muted-foreground" />
+            </div>
+            <h3 class="font-medium">{city}</h3>
+            <Badge look={"outline"}>{_count.groups} Groups</Badge>
+          </Card.Content>
+        </Card.Root>
+      </Link>
+    );
+  },
+);
--- src/routes/(unauthenticated)/popular-cities.tsx
+++ src/routes/(unauthenticated)/popular-cities.tsx
@@ -1,9 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { LuBuilding2 } from "@qwikest/icons/lucide";
-import { Card } from "~/components/ui/card/card";
 import { useGetPopularCities } from ".";
-import { Badge } from "~/components/ui/badge/badge";
 import { Link } from "@builder.io/qwik-city";
+import { LocationCard } from "~/components/shared/location-card";
 export const PopularCities = component$(() => {
   const popularCities = useGetPopularCities();
   return (
@@ -16,18 +14,8 @@ export const PopularCities = component$(() => {
       </div>
 
       <div class="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-        {popularCities.value?.map(({ id, city, _count }) => (
-          <Link key={id} href={`/cities/${id}`}>
-            <Card.Root class="max-w-md">
-              <Card.Content class="m-0 flex flex-col items-center gap-3 p-4">
-                <div class="grid h-36 w-36 place-items-center rounded-full bg-muted">
-                  <LuBuilding2 class="h-16 w-16 text-muted-foreground" />
-                </div>
-                <h3 class="font-medium">{city}</h3>
-                <Badge look={"outline"}>{_count.groups} Groups</Badge>
-              </Card.Content>
-            </Card.Root>
-          </Link>
+        {popularCities.value?.map((location) => (
+          <LocationCard location={location} key={location.id} />
         ))}
       </div>
     </section>
