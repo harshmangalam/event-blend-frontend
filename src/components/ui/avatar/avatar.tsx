2a75be534af7686e19928b770a26ed2dd603593c
harshmangalam <harshdev8218@gmail.com>
2024-07-25 11:40:42 +0530

    added avatar component

---
+++ src/components/ui/avatar/avatar.tsx
@@ -0,0 +1,40 @@
+import { PropsOf, Slot, component$ } from "@builder.io/qwik";
+import { cn } from "@qwik-ui/utils";
+
+const Root = component$<PropsOf<"div">>(({ ...props }) => {
+  return (
+    <div
+      {...props}
+      class={cn(
+        "relative flex h-10 w-10 shrink-0 items-center justify-center overflow-hidden rounded-full",
+        props.class,
+      )}
+    >
+      <Slot />
+    </div>
+  );
+});
+
+const Image = component$<PropsOf<"img">>(({ ...props }) => (
+  <img {...props} class={cn("aspect-square h-full w-full", props.class)} />
+));
+
+const Fallback = component$<PropsOf<"div">>(({ ...props }) => {
+  return (
+    <div
+      {...props}
+      class={cn(
+        "absolute -z-10 flex h-full w-full items-center justify-center rounded-full bg-muted",
+        props.class,
+      )}
+    >
+      <Slot />
+    </div>
+  );
+});
+
+export const Avatar = {
+  Root,
+  Image,
+  Fallback,
+};
