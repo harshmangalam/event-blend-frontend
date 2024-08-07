387b8f6d77f19be980c8dda7191acc49e2c804e7
harshmangalam <harshdev8218@gmail.com>
2024-08-07 12:19:59 +0530

    updated navbar

--- src/components/navigations/navbar.tsx
+++ src/components/navigations/navbar.tsx
@@ -3,14 +3,13 @@ import { buttonVariants } from "../ui/button/button";
 import { Link } from "@builder.io/qwik-city";
 import { cn } from "@qwik-ui/utils";
 import { SITE_NAME } from "~/lib/constatnts";
-import { ThemeSwitcher } from "../theme-switcher";
 
 export const Navbar = component$(() => {
   return (
-    <header class="flex h-16 items-center">
-      <nav class="container mx-auto flex max-w-6xl flex-1 items-center justify-between gap-4 px-4 ">
+    <header class="sticky top-0 z-50 flex h-16 items-center border-b border-muted bg-background">
+      <nav class="container mx-auto flex flex-1 items-center justify-between gap-4 px-4">
         <div>
-          <h1 class="text-2xl font-bold">{SITE_NAME}</h1>
+          <h1 class="text-2xl font-bold ">{SITE_NAME}</h1>
         </div>
         <div class="flex items-center gap-2">
           <Link
@@ -25,7 +24,6 @@ export const Navbar = component$(() => {
           >
             Sign up
           </Link>
-          <ThemeSwitcher />
         </div>
       </nav>
     </header>
