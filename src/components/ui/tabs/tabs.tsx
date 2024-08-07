69e60b28cce79f3c5a85897d9aa10dfc4a93b004
harshmangalam <harshdev8218@gmail.com>
2024-08-07 13:54:05 +0530

    added tabs

---
+++ src/components/ui/tabs/tabs.tsx
@@ -0,0 +1,61 @@
+import { Slot, component$, PropsOf } from "@builder.io/qwik";
+import { Tabs as HeadlessTabs } from "@qwik-ui/headless";
+import { cn } from "@qwik-ui/utils";
+
+const Root = (props: PropsOf<typeof HeadlessTabs.Root>) => (
+  <HeadlessTabs.Root
+    {...props}
+    tabListComponent={List}
+    tabComponent={Tab}
+    tabPanelComponent={Panel}
+  />
+);
+
+const List = component$<PropsOf<typeof HeadlessTabs.List>>((props) => {
+  return (
+    <HeadlessTabs.List
+      {...props}
+      class={cn(
+        "inline-flex items-center justify-center rounded-lg border-base bg-muted p-1 text-muted-foreground shadow-sm",
+        props.class,
+      )}
+    >
+      <Slot />
+    </HeadlessTabs.List>
+  );
+});
+
+const Tab = component$<PropsOf<typeof HeadlessTabs.Tab>>((props) => {
+  return (
+    <HeadlessTabs.Tab
+      {...props}
+      class={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-2 font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=selected]:border-base data-[state=selected]:bg-background data-[state=selected]:text-foreground data-[state=selected]:shadow-inner",
+        props.class,
+      )}
+    >
+      <Slot />
+    </HeadlessTabs.Tab>
+  );
+});
+
+const Panel = component$<PropsOf<typeof HeadlessTabs.Panel>>((props) => {
+  return (
+    <HeadlessTabs.Panel
+      {...props}
+      class={cn(
+        "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+        props.class,
+      )}
+    >
+      <Slot />
+    </HeadlessTabs.Panel>
+  );
+});
+
+export const Tabs = {
+  Root,
+  List,
+  Tab,
+  Panel,
+};
