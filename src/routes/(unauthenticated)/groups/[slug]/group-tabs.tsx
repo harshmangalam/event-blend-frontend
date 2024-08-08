89dc163679c72522da1a07aa5874ab67a9e1e37e
harshmangalam <harshdev8218@gmail.com>
2024-08-08 14:09:12 +0530

    added groups tabs

--- src/routes/(unauthenticated)/groups/[slug]/group-tabs.tsx
+++ src/routes/(unauthenticated)/groups/[slug]/group-tabs.tsx
@@ -1,16 +1,24 @@
 import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 import { NavLink } from "~/components/nav-link";
 import { GROUP_DETAILS_TABS } from "~/lib/constatnts";
 
 export const GroupTabs = component$(() => {
+  const location = useLocation();
   return (
     <div class="bg-background">
-      <div class="container mx-auto flex h-16 max-w-5xl flex-wrap items-center gap-4 px-4">
-        {GROUP_DETAILS_TABS.map((tab) => (
-          <NavLink key={tab.slug} href={tab.slug} activeClass="text-primary">
-            {tab.name}
-          </NavLink>
-        ))}
+      <div class="container mx-auto flex h-16 max-w-5xl flex-wrap items-center gap-8 px-4">
+        {GROUP_DETAILS_TABS.map((tab) => {
+          const href =
+            tab.slug === ""
+              ? `/groups/${location.params.slug}/`
+              : `/groups/${location.params.slug}/${tab.slug}/`;
+          return (
+            <NavLink key={tab.slug} href={href} activeClass="text-primary">
+              {tab.name}
+            </NavLink>
+          );
+        })}
       </div>
     </div>
   );
