8e2ae1a09cc0926d81379bf35a440fb8ff545203
harshmangalam <harshdev8218@gmail.com>
2024-08-08 12:32:39 +0530

    Added home page resposive

--- src/routes/(unauthenticated)/index.tsx
+++ src/routes/(unauthenticated)/index.tsx
@@ -54,7 +54,7 @@ export const useGetPopularEvents = routeLoader$(async () => {
 });
 export default component$(() => {
   return (
-    <div class="grid w-full grid-cols-1 gap-16">
+    <div class="container mx-auto grid w-full grid-cols-1 gap-16 px-4">
       <Hero />
       <PopularEvents />
       <PopularCategories />
