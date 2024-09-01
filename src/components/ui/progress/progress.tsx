0b72a0e205e82aac5614c441e538ea3e03ffb520
harshmangalam <harshdev8218@gmail.com>
2024-09-01 13:32:06 +0530

    added textarea

---
+++ src/components/ui/progress/progress.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@qwik-ui/utils";
+import { type PropsOf, component$ } from "@builder.io/qwik";
+import { Progress as HeadlessProgress } from "@qwik-ui/headless";
+
+export const Progress = component$<PropsOf<typeof HeadlessProgress.Root>>(
+  (props) => {
+    return (
+      <HeadlessProgress.Root
+        class={cn(
+          "relative h-3 w-full overflow-hidden rounded bg-muted",
+          props.class,
+        )}
+      >
+        <HeadlessProgress.Indicator
+          class="h-full w-full flex-1 bg-primary transition-all"
+          style={{ transform: `translateX(-${100 - (props.value || 0)}%)` }}
+        />
+      </HeadlessProgress.Root>
+    );
+  },
+);
