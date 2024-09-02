639e2156804803aa018e029fb3ccfe99872bb253
harshmangalam <harshdev8218@gmail.com>
2024-09-03 00:26:25 +0530

    created group progress indicator

--- src/routes/start-group/group-progress-status.tsx
+++ src/routes/start-group/group-progress-status.tsx
@@ -1,6 +1,24 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 import { Progress } from "~/components/ui/progress/progress";
 
 export const GroupProgressStatus = component$(() => {
-  return <Progress value={30} class="rounded-none" />;
+  const location = useLocation();
+  const statusObject: { [key: string]: number } = {
+    "basic-info": 40,
+    category: 60,
+    topics: 80,
+    location: 100,
+  };
+
+  const progressSig = useSignal(0);
+
+  useTask$(() => {
+    Object.keys(statusObject).map((path) => {
+      if (location.url.pathname.includes(path)) {
+        progressSig.value = statusObject[path];
+      }
+    });
+  });
+  return <Progress value={progressSig.value} class="rounded-none" />;
 });
