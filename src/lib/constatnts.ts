6a3e5ab9a850331443b5cf967bfd75d2b3587500
harshmangalam <harshdev8218@gmail.com>
2024-09-17 14:49:13 +0530

    updated logo text

--- src/lib/constatnts.ts
+++ src/lib/constatnts.ts
@@ -4,7 +4,7 @@ const REFRESH_TOKEN_KEY = "accessToken";
 const ACCESS_TOKEN_EXP = 24 * 60 * 60; //  1 day
 const REFRESH_TOKEN_EXP = 30 * 86400; // 30 days
 const SITE_SHORT_NAME = "EB";
-const SITE_NAME = "Event Blend";
+const SITE_NAME = "Eventblend";
 const DEFAULT_POSTER =
   "https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
