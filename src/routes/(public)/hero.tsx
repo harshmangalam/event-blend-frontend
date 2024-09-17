ddf0f0837d9791a7a127bc0eb0b8b5124695c30c
harshmangalam <harshdev8218@gmail.com>
2024-09-17 15:07:19 +0530

    updated hero

--- src/routes/(public)/hero.tsx
+++ src/routes/(public)/hero.tsx
@@ -6,40 +6,43 @@ import { SITE_NAME } from "~/lib/constatnts";
 
 export const Hero = component$(() => {
   return (
-    <div class="py-12">
-      <div class="relative w-full">
-        <div class="flex flex-col items-center sm:flex-row">
-          <div class="mb-6 flex w-full flex-col space-y-6 sm:mb-0 sm:mr-6 lg:w-7/12">
-            <h1 class="text-[42px] font-extrabold leading-[3.25rem]">
-              The Open Source Platform for Organizing and Attending Events
-            </h1>
-            <p>
-              Discover and create events effortlessly with {SITE_NAME}. Our open
-              source platform empowers you to connect with communities, share
-              experiences, and make memories. Join us and transform the way you
-              engage with events. fun.
-            </p>
-            <div>
-              <Link
-                class={cn(buttonVariants({ size: "md", look: "primary" }))}
-                href="/signup"
-              >
-                Join Now
-              </Link>
-            </div>
+    <div class="relative w-full">
+      <div class="flex flex-col items-center sm:flex-row">
+        <div class="mb-6 flex w-full flex-col space-y-6 sm:mb-0 sm:mr-6 lg:w-2/3 xl:w-7/12">
+          <h1 class="text-3xl font-extrabold tracking-tight lg:text-4xl xl:text-[42px] xl:leading-[3.5rem]">
+            An open source platform for organizing and attending events
+          </h1>
+          <p>
+            Discover and create events effortlessly with {SITE_NAME}. Our open
+            source platform empowers you to connect with communities, share
+            experiences, and make memories. Join us and transform the way you
+            engage with events. fun.
+          </p>
+          <div>
+            <Link
+              class={cn(
+                buttonVariants({
+                  size: "md",
+                  look: "primary",
+                  class: "px-6",
+                }),
+              )}
+              href="/signup"
+            >
+              Join {SITE_NAME}
+            </Link>
           </div>
-          <div class="w-8/12 lg:w-1/2">
-            <img
-              alt="Image of an IRL Event"
-              fetchPriority="high"
-              width="379"
-              height="269"
-              decoding="async"
-              data-nimg="1"
-              srcset="https://secure.meetupstatic.com/next/images/indexPage/irl_event.svg?w=384 1x, https://secure.meetupstatic.com/next/images/indexPage/irl_event.svg?w=828 2x"
-              src="https://secure.meetupstatic.com/next/images/indexPage/irl_event.svg?w=828"
-            />
-          </div>
+        </div>
+        <div class="w-full lg:w-1/3 xl:w-1/2">
+          <img
+            alt="Image of an IRL Event"
+            fetchPriority="high"
+            width="460"
+            height="400"
+            decoding="async"
+            data-nimg="1"
+            src="hero.png"
+          />
         </div>
       </div>
     </div>
