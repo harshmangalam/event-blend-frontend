3fa365d95e2c8b6fbf320b0dda82bae10af558b7
harshmangalam <harshdev8218@gmail.com>
2024-09-19 13:10:35 +0530

    added skelton component

---
+++ src/components/ui/skeleton/skeleton.tsx
@@ -0,0 +1,11 @@
+import { type PropsOf, component$ } from "@builder.io/qwik";
+import { cn } from "@qwik-ui/utils";
+
+export const Skeleton = component$<PropsOf<"div">>(({ ...props }) => {
+  return (
+    <div
+      {...props}
+      class={cn("bg-foreground/10 animate-pulse rounded", props.class)}
+    />
+  );
+});
