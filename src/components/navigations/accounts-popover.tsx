ebf7df637bb02995753896dbacffb86f3d98f363
harshmangalam <harshdev8218@gmail.com>
2024-09-19 13:52:20 +0530

    added account popover

---
+++ src/components/navigations/accounts-popover.tsx
@@ -0,0 +1,63 @@
+import { component$ } from "@builder.io/qwik";
+import { Form, Link } from "@builder.io/qwik-city";
+import { Avatar, Button, Popover, Separator } from "~/components/ui";
+import type { AuthUser } from "~/lib/types";
+
+export const AccountsPopover = component$(({ user }: { user: AuthUser }) => {
+  const links = [
+    {
+      name: "Your events",
+      href: "/your-events",
+    },
+    {
+      name: "Your groups",
+      href: "/your-groups",
+      divider: true,
+    },
+    {
+      name: "View profile",
+      href: `/members/${user.id}`,
+    },
+    {
+      name: "Settings",
+      href: `/account`,
+    },
+  ];
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <Avatar.Root class="h-10 w-10">
+          <Avatar.Image src={user.profilePhoto ?? ""} alt={user.name} />
+          <Avatar.Fallback>{user.name}</Avatar.Fallback>
+        </Avatar.Root>
+      </Popover.Trigger>
+      <Popover.Panel class="mt-4 w-48">
+        <ul class="flex flex-col gap-2">
+          {links.map((link) => (
+            <>
+              <li key={link.name} class="w-full">
+                <Link
+                  class="text-sm opacity-80 hover:text-primary hover:opacity-100"
+                  href={link.href}
+                >
+                  {link.name}
+                </Link>
+              </li>
+              {link.divider && <Separator />}
+            </>
+          ))}
+          <li>
+            <Form>
+              <button
+                class="text-sm opacity-80 hover:text-primary hover:opacity-100"
+                type="submit"
+              >
+                Log out
+              </button>
+            </Form>
+          </li>
+        </ul>
+      </Popover.Panel>
+    </Popover.Root>
+  );
+});
--- src/components/navigations/navbar.tsx
+++ src/components/navigations/navbar.tsx
@@ -3,15 +3,19 @@ import { buttonVariants } from "../ui/button/button";
 import { Link } from "@builder.io/qwik-city";
 import { cn } from "@qwik-ui/utils";
 import { Logo } from "../shared/logo";
-import { useUser } from "~/routes/plugin@auth";
+
+import { AccountsPopover } from "./accounts-popover";
+import { useSession } from "~/routes/plugin@auth";
 
 export const Navbar = component$(() => {
-  const userSig = useUser();
+  const sessionSig = useSession();
   return (
     <header class="sticky top-0 z-50 flex h-16 items-center border-b border-muted bg-background">
       <nav class="container mx-auto flex flex-1 items-center justify-between gap-4 px-4">
         <Logo />
-        {userSig.value && (
+        {sessionSig.value.user ? (
+          <AccountsPopover user={sessionSig.value.user} />
+        ) : (
           <div class="flex items-center gap-2">
             <Link
               href="/login"
