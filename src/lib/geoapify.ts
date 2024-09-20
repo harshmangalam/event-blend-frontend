8860be010f443d8a60a2a53a768d3cd89bc77003
harshmangalam <harshdev8218@gmail.com>
2024-09-20 12:36:06 +0530

    added geolocation lib

---
+++ src/lib/geoapify.ts
@@ -0,0 +1,23 @@
+import wretch from "wretch";
+
+export type GeoapifyLocation = {
+  place_id: string;
+  country: string;
+  state: string;
+  city: string;
+  lon: string;
+  lat: string;
+  name: string;
+};
+export async function autocompleteLocation(text: string, apiKey?: string) {
+  if (!apiKey) return [];
+  if (!text.trim().length) return [];
+  const resp = await wretch(
+    `https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&format=json&apiKey=${apiKey}`,
+  )
+    .get()
+    .fetchError((err) => console.log(err))
+    .json<{ results: GeoapifyLocation[] }>();
+
+  return resp.results.filter((r) => r.city || r.name);
+}
