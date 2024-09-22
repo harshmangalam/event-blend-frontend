c02333062522e45087d89cecfd24259975f0d0b7
harshmangalam <harshdev8218@gmail.com>
2024-09-22 11:01:21 +0530

    updated layout

--- src/routes/(authenticated)/layout.tsx
+++ src/routes/(authenticated)/layout.tsx
@@ -1,5 +1,15 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { Footer } from "~/components/footer";
+import { Navbar } from "~/components/navigations/navbar";
 
 export default component$(() => {
-  return <Slot />;
+  return (
+    <div class="flex min-h-screen flex-col bg-muted">
+      <Navbar />
+      <main class="grid flex-1 place-items-center py-12">
+        <Slot />
+      </main>
+      <Footer />
+    </div>
+  );
 });
