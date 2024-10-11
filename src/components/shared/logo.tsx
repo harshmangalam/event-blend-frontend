6444e7766e873f565c66b66d39b0aedf2d8f7824
SRIJAN KUMAR SAH <65221765+Srijan25@users.noreply.github.com>
2024-10-11 23:51:15 +0530

    Update logo.tsx

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -4,6 +4,8 @@ import { SITE_NAME } from "~/lib/constatnts";
 
 export const Logo = component$(() => (
   <Link href="/">
-    <h1 class="text-2xl font-extrabold italic text-alert">{SITE_NAME}</h1>
+    {/* <h1 class="text-2xl font-extrabold italic text-alert">{SITE_NAME}</h1> */}
+    <img src="/logo.svg" alt={SITE_NAME} class="w-28" />
   </Link>
+
 ));
