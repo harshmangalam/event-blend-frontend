dfc07f63997f83e31cf3cea223236586df6841ae
harshmangalam <harshdev8218@gmail.com>
2024-10-12 16:32:26 +0530

    added home page

---
+++ src/routes/(authenticated)/home/index.tsx
@@ -0,0 +1,17 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div>
+      {/* username welcome title  */}
+
+      <div>
+        {/* left section  */}
+        <section></section>
+
+        {/* right section  */}
+        <section></section>
+      </div>
+    </div>
+  );
+});
