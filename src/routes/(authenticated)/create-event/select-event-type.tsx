68a88ab0c1cc17245728d57f73ac1c3a0ce114cb
SureshPradhana <sureshpradhana19@gmail.com>
2024-10-12 17:33:55 +0530

    issue_64:change label to In Person

--- src/routes/(authenticated)/create-event/select-event-type.tsx
+++ src/routes/(authenticated)/create-event/select-event-type.tsx
@@ -1,13 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 
 export const SelctEventType = component$(() => {
-  const eventTypes = ["Online", "InPerson"];
+  const eventTypes = [
+    { label: "Online", value: "Online" },
+    { label: "In Person", value: "InPerson" },
+  ];
   return (
     <select id="eventType" name="eventType" class="rounded-md border px-4 py-3">
       <option value={""}>Select</option>
-      {eventTypes.map((eventType) => (
-        <option value={eventType} key={eventType}>
-          {eventType}
+      {eventTypes.map(({ label, value}) => (
+        <option value={label} key={value}>
+          {label}
         </option>
       ))}
     </select>
