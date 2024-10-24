b4b878a33882cb9548129fc73c591eee1e2b492a
harshmangalam <harshdev8218@gmail.com>
2024-10-25 00:32:18 +0530

    fixed group tabs

--- src/lib/constatnts.ts
+++ src/lib/constatnts.ts
@@ -8,20 +8,6 @@ const SITE_NAME = "Eventblend";
 const DEFAULT_POSTER =
   "https://images.unsplash.com/photo-1502444330042-d1a1ddf9bb5b?q=80&w=2946&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-const GROUP_DETAILS_TABS = [
-  {
-    name: "About",
-    slug: "",
-  },
-  {
-    name: "Events",
-    slug: "events",
-  },
-  {
-    name: "Members",
-    slug: "members",
-  },
-];
 const REDIRECT_STATUS_CODE = 302;
 
 const GEOAPIFY_API_KEY = "GEOAPIFY_API_KEY";
@@ -55,7 +41,6 @@ export {
   REFRESH_TOKEN_EXP,
   SITE_NAME,
   DEFAULT_POSTER,
-  GROUP_DETAILS_TABS,
   SITE_SHORT_NAME,
   REDIRECT_STATUS_CODE,
   ACCESS_TOKEN_KEY,
